eb9ea86af3f31d955c6034dca4eec9b34b4fee19
Shreyas Canchi Radhakrishna <shreyas.rad@gmail.com>
2016-02-05 00:14:04 -0800

    Main.cpp can read files

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,25 @@
 // Main code here
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std; 
+
+int main(int argc, char** argv){
+	//cout << "STUB" << endl;
+	string line;
+	ifstream myfile ("input.txt");
+
+	if (myfile.is_open()){
+  		while ( getline (myfile,line))
+    		cout << line << '\n';
+   
+    	myfile.close();
+	}
+
+	else cout << "Unable to open file"; 	
+
+
+	return 0; 
+}
