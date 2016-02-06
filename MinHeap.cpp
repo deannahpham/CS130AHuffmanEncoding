3c581f6cc1ab10275d66a7edb27d1fe67e5df697
Deanna <deannahpham@umail.ucsb.edu>
2016-02-05 20:38:11 -0800

    Changed MinHeap implementation to array

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -1,2 +1,31 @@
 // MinHeap implementation
 #include "MinHeap.h"
+#include <cstddef>
+
+
+MinHeap::MinHeap() {
+	
+	heap = new Trie[32];
+	nextAdd = 1; 
+}
+
+bool MinHeap::add(Trie* t) {
+
+	
+	return false;
+}
+
+Trie* MinHeap::peek(){
+
+	return NULL;
+}
+
+Trie* MinHeap::poll(){
+	
+	return NULL;
+}
+
+void MinHeap::print(){
+
+}
+
--- MinHeap.h
+++ MinHeap.h
@@ -1,16 +1,25 @@
 #ifndef MINHEAP_H
 #define MINHEAP_H
 
-struct Node{
+struct Trie{
 	char c;
 	int frequency;
+	Trie* left;
+	Trie* right;
 };
+
 
 class MinHeap{
 public: 
-
+	MinHeap(); //constructor
+	bool add(Trie* t); // add node N to heap
+	Trie* peek(); //returns root but does not remove it
+	Trie* poll(); // return and remove root
+	void print(); //print heap
 
 private:
+	Trie* heap; //root of heap
+	int nextAdd; //keep track of the next index of where we want to add 
 
 };
 
